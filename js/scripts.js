b53d5c02a143af594298218386154990f541d365
RS <roboszyn@gmail.com>
2017-02-24 02:30:57 +0000

    comments added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // plik scripts.js
 
+
+//funkcja poniżej spowoduje wyskoczenie okna, w którym będziemy musieli podać swoje imię
 var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+
+// alert('Witaj, ' + name); (wyłączenie funkcji)
+
+/*wyłączenie funkcji
+console.log('Witaj, ' + name);*/
